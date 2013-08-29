f8b028a196b2eadcd47b039494474c48cf92ad96
Carl Grundberg <carl.grundberg@menmo.se>
2013-08-29 16:46:32 +0200

    Jquery ported to Angular

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,53 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('pivoter.controllers', []).
+    controller('GridCtrl', function ($scope, items, socket) {
+        $scope.selectedValue = null;
+        $scope.items = items;
 
-  }])
-  .controller('MyCtrl2', [function() {
+        socket.emit('voter');
 
-  }]);
+        $scope.vote = function(value) {
+            $scope.selectedValue = value;
+            socket.emit('vote', { value: value });
+        };
+
+        socket.on('update', function(state) {
+            if($scope.selectedValue != null && !state.voters[socket.id()]) {
+                $scope.selectedValue = null;
+            }
+        });
+    }).
+    controller('ResultsCtrl', function ($scope, items, socket) {
+        $scope.result = false;
+
+        socket.on('update', function(state) {
+            $scope.state = state;
+        });
+
+        $scope.reset = function() {
+            $scope.result = false;
+            socket.emit('reset');
+        };
+
+        $scope.show = function() {
+            var bars = {};
+            for(var i in $scope.state.voters) {
+                if($scope.state.voters[i] !== null) {
+                    if(bars[$scope.state.voters[i]]) {
+                        bars[$scope.state.voters[i]]++;
+                    } else {
+                        bars[$scope.state.voters[i]] = 1;
+                    }
+                }
+            }
+            $scope.items = items;
+            $scope.result = bars;
+        };
+    }).
+    controller('FooterCtrl', function ($scope, socket) {
+        socket.on('update', function(state) {
+            $scope.state = state;
+        });
+    });
--- app/js/services.js
+++ app/js/services.js
@@ -2,8 +2,31 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('pivoter.services', []).
+  value('items', ['0', 1, 2, 3, 5, 8, 13, 21, 34, 55, '?', '<img src="img/coffe_cup.png" />']).
+  factory('socket', function ($rootScope) {
+    var socket = io.connect();
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        },
+        id: function() {
+            return socket.socket.sessionid;
+        }
+    };
+});
